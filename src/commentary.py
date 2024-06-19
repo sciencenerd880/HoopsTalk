e11063cd14f83340771b617732e98c3f87c514dd
Daniel James <danieljames96@gmail.com>
2024-06-19 23:30:22 +0800

    Added probability of action to commentary

--- src/commentary.py
+++ src/commentary.py
@@ -5,13 +5,18 @@ from dotenv import load_dotenv
 import pandas as pd
 from video_processor import extract_encode_frames, create_message
 from openai_client import get_commentary_for_frames
+from tqdm import tqdm
+import re
 
 def main():
     load_dotenv()
 
-    frame_interval = 60
-    video_dir = './data/raw/NSVA_Video/0021800013-dal-vs-phx'
-    text_dir = './data/text/GPT4o/commentary_results.csv'
+    frame_interval = 30
+    metadata_file = './data/raw/NSVA_Data_Exploded/0021800013-dal-vs-phx-exploded.xlsx'
+    match = re.search(r'/([^/]+)-exploded\.xlsx$', metadata_file)
+    game = match.group(1)
+    video_dir = './data/raw/NSVA_Video/' + game
+    text_dir = f'./data/text/GPT4o/{game}_commentary_results.csv'
     api_key = os.getenv('OPENAI_API_KEY')
 
     if not api_key:
@@ -19,21 +24,26 @@ def main():
         return
 
     # Convert the directory string to a Path object
-    abs_video_dir = Path(video_dir).resolve()
-    logging.info(f"Absolute video directory: {abs_video_dir}")
+    # abs_video_dir = Path(video_dir).resolve()
+    # logging.info(f"Absolute video directory: {abs_video_dir}")
 
     results = []
     commentary_buffer = []  # Initialize a buffer to store the last 10 lines of commentary
+    
+    meta_df = pd.read_excel(metadata_file)
+    meta_df = meta_df.loc[meta_df['ActionType1_Pred_Result'].isin(['Foul','Rebound','Made Shot','Missed Shot','Turnover'])]
+    meta_df = meta_df.iloc[:10,:]
 
-    # Use glob to find all .mp4 files in the directory
-    for video_file in abs_video_dir.glob('*.mp4'):
+    for index, row in tqdm(meta_df.iterrows()):
+        video_file = video_dir + '/' + game + '_' + str(row['video_id']) + '.mp4'
         logging.info(f"Processing video: {video_file}")
         base64_frames = extract_encode_frames(video_file, frame_interval)
-        messages = create_message(base64_frames, frame_interval, "\n".join(commentary_buffer))
+        pred_actions = (row['ActionType1_Pred_Result'],row['ActionType1_Pred_Prob'],row['ActionType2_Pred_Result'],row['ActionType2_Pred_Prob'])
+        messages = create_message(base64_frames, pred_actions, frame_interval, "\n".join(commentary_buffer))
         commentary = get_commentary_for_frames(api_key, messages)
         
         if commentary:
-            results.append({'video_file': video_file.name, 'commentary': commentary})
+            results.append({'video_id': row['video_id'], 'gen_commentary': commentary})
             commentary_buffer.extend(commentary.split("\n"))  # Update the buffer with new lines
             commentary_buffer = commentary_buffer[-10:]  # Keep only the last 10 lines
         else:
@@ -41,9 +51,10 @@ def main():
 
     # Save results to CSV
     df = pd.DataFrame(results)
-    df.to_csv(text_dir, index=False)
+    merged_df = pd.merge(meta_df, df, on='video_id', how='left')
+    merged_df.to_csv(text_dir, index=False)
     logging.info(f"Commentary saved to {text_dir}")
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO) # INFO, CRITICAL
     main()
--- src/video_processor.py
+++ src/video_processor.py
@@ -24,26 +24,50 @@ def extract_encode_frames(video_path, frame_interval=50):
     
     return base64_frames
 
-def create_message(encoded_frames, frame_interval=50, previous_commentary=""):
+def create_message(encoded_frames, pred_actions, frame_interval=50, previous_commentary=""):
+    ActionType1_Pred_Result, ActionType1_Pred_Prob, ActionType2_Pred_Result, ActionType2_Pred_Prob = pred_actions
+    event_string = ""
+    
+    if ActionType1_Pred_Result != 'NA':
+        event_string += f"ActionType1: {ActionType1_Pred_Result} with a probability of {ActionType1_Pred_Prob:.2f}. "
+    if ActionType2_Pred_Result != 'NA':
+        event_string += f"ActionType2: {ActionType2_Pred_Result} with a probability of {ActionType2_Pred_Prob:.2f}. "
+        
     content = [
-        "You are an NBA commentator. Here are frames from a recent NBA game. Your task is to provide a brief and engaging commentary summarizing the key events depicted in these frames. Please maintain a flow and continuity with the previous commentary provided. Ensure that the events described are logically sequenced and contextually appropriate for different parts of the game:",
+        """
+        You are an enthusiastic and knowledgeable NBA commentator for the 2018-2019 season. Here are frames from a recent NBA game. 
+        Your task is to provide a brief and engaging commentary summarizing the key events depicted in these frames. Maintain a flow and continuity 
+        with the previous commentary provided. Ensure that the events described are logically sequenced and contextually appropriate for different parts of the game:
+        """,
         *map(lambda x: {"image": x, "resize": 768}, encoded_frames[0::frame_interval]),
         f"Previous commentary:\n{previous_commentary}",
-        "Consider the following types of events to help structure your commentary:",
+        f"Event context:\n{event_string}",
         """
-        1. Play Start: Describe the beginning of an offensive or defensive play.
-        2. Shot Attempt: [Player] attempts a [shot type] from [distance].
-        3. Rebound: [Player] grabs the rebound.
-        4. Successful Shot: [Player] scores a [shot type] from [distance].
-        5. Assist: Assist by [Player].
-        6. Foul: [Player] commits a [foul type].
-        7. Turnover: [Player] loses the ball.
-        8. Significant Moment: Describe any significant moment like a game-changing play or a remarkable defensive action.
-        9. End of Period: End of [period number].
-        Combine these elements to provide a cohesive and engaging summary of the key events depicted in these frames in approximately two sentences.
-        """
+        Consider the following types of events to help structure your commentary. Replace all words in brackets with the appropriate entity:
+        
+        1. Missed Shot: Replace [Player] with the player's name (identified by jersey number or team) attempting a [shot type].
+        2. Rebound: Replace [Player] with the player's name (identified by jersey number or team) grabbing the rebound.
+        3. Made Shot: Replace [Player] with the player's name (identified by jersey number or team) scoring a [shot type].
+        4. Foul: Replace [Player] with the player's name (identified by jersey number or team) committing a [foul type].
+        5. Turnover: Replace [Player] with the player's name (identified by jersey number or team) losing the ball.
+
+        Ensure your commentary identifies players either by their name, their jersey number, or their team, and integrates these elements to provide a cohesive and engaging summary of the key events depicted in these frames in 15 words.
+        """,
+        "Ensure your commentary captures the excitement and energy of the game, providing vivid and dynamic descriptions of the actions and players involved. Your goal is to make the viewers feel as if they are witnessing the events unfold in real-time."
     ]
     
     messages = [{"role": "user", "content": content}]
     
     return messages
+
+'''
+1. Play Start: Describe the beginning of an offensive or defensive play.
+        2. Shot Attempt: Replace [Player] with the player's name (identified by jersey number or team) attempting a [shot type] from [distance].
+        3. Rebound: Replace [Player] with the player's name grabbing the rebound.
+        4. Successful Shot: Replace [Player] with the player's name scoring a [shot type] from [distance].
+        5. Assist: Replace [Player] with the player's name who assists.
+        6. Foul: Replace [Player] with the player's name committing a [foul type].
+        7. Turnover: Replace [Player] with the player's name losing the ball.
+        8. Significant Moment: Describe any significant moment like a game-changing play or a remarkable defensive action, replacing [Player] with the relevant player's name.
+        9. End of Period: Indicate the end of [period number].
+'''
